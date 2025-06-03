a50dabdb32474593a8db8efb61caaab005e6957c
juampymdp <juampymdp@gmail.com>
2025-06-03 13:35:05 +0200

    done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,41 +1,61 @@
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
-
-    <div class="body">
-      <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
-
-        <span class="timestamp">Nov 30, 2020</span>
-      </div>
-
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
+    <div class="tweet-header">
+      <img :src="tweet.user.avatar" alt="Avatar" class="avatar" />
+      <div class="user-info">
+        <strong>{{ tweet.user.name }}</strong>
+        <span>@{{ tweet.user.handle }}</span>
       </div>
     </div>
-
-    <i class="fas fa-ellipsis-h"></i>
+    <p class="tweet-content">{{ tweet.content }}</p>
+    <div class="tweet-footer">
+      <span>{{ tweet.likes }} Likes</span>
+      <span>{{ tweet.retweets }} Retweets</span>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Tweet',
+  props: {
+    tweet: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
 <style scoped>
-a {
-  color: #42b983;
+.tweet {
+  border: 1px solid #e1e8ed;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 8px;
+  background-color: #fff;
+}
+.tweet-header {
+  display: flex;
+  align-items: center;
+}
+.avatar {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  margin-right: 0.5rem;
+}
+.user-info {
+  display: flex;
+  flex-direction: column;
+}
+.tweet-content {
+  margin: 1rem 0;
+}
+.tweet-footer {
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.9rem;
+  color: #657786;
 }
 </style>
---
+++ src/components/TweetList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Tweet v-for="tweet in tweets" :key="tweet.id" :tweet="tweet" />
+  </div>
+</template>
+
+<script>
+import Tweet from './Tweet.vue';
+
+export default {
+  name: 'TweetList',
+  components: {
+    Tweet
+  },
+  props: {
+    tweets: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
